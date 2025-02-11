8ff2d087f2cc132f19925031fccb526b5f8f5fd7
nicolasKappa <nicola.kappa8@gmail.com>
2025-02-11 17:32:10 +0400

    Starting Authentication Process for Log-In

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
 const User = require('./models/user');
 
 const app = express();
@@ -14,14 +14,15 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  User.findById('67a60e8dc07bfbc7b7dfe53c')
+  User.findById('67a89d930e3837b5827bf74d')
     .then(user => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
+      req.user = user;
       next();
     })
     .catch(err => console.log(err));
@@ -29,9 +30,28 @@ app.use((req, res, next) => {
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-  app.listen(3000);
-});
+mongoose
+  .connect(
+'mongodb+srv://Username:Password@cluster0.jkcn0.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0')  
+  .then(result => {
+    User.findOne().then(user => {
+      if (!user) {
+        const user = new User({
+          name: 'Max',
+          email: 'max@test.com',
+          cart: {
+            items: []
+          }
+        });
+        user.save();
+      }
+    });
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
---
+++ controllers/auth.js
@@ -0,0 +1,19 @@
+exports.getLogin = (req, res, next) => {
+  // const isLoggedIn = req
+  // .get('Cookie')
+  // .split(';')[1]
+  // .trim()
+  // .split('=')[1] === 'true';
+      res.render('auth/login', {
+        path: '/login',
+        pageTitle: 'Login',
+        isAuthenticated: false
+
+      });
+};
+
+exports.postLogin = (req, res, next) => {
+    res.setHeader('Set-Cookie', 'loggedIn=true; Secure');
+    res.redirect('/');
+
+};
