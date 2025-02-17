01f531ff9344201653cc84d9467cbc586a8ca503
nicolasKappa <nicola.kappa8@gmail.com>
2025-02-18 00:55:20 +0400

    Improve#1

--- controllers/auth.js
+++ controllers/auth.js
@@ -10,7 +10,7 @@ const User = require('../models/user');
 const transporter = nodemailer.createTransport(
   sendgridTransport({
     auth: {
-      api_key:
+      check:
       process.env.SENDGRID_API_KEY
     }
   })
